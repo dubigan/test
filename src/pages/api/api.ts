ff971306d64b3fe72c2876c8b84285c10894d4e5
oak <353991@gmail.com>
2021-08-15 21:57:34 +0300

    CORS

--- src/pages/api/api.ts
+++ src/pages/api/api.ts
@@ -1,24 +1,24 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BASE_URL = "https://test-php.herokuapp.com/";
+const BASE_URL = 'https://pskov-php.herokuapp.com/';
 // const BASE_URL = 'https://localhost:8002/';
 
 const instance = axios.create({
-    baseURL: BASE_URL,
-    method: "POST",
-    headers: {
-        "content-type": "application/json",
-    },
+  baseURL: BASE_URL,
+  method: 'POST',
+  headers: {
+    'content-type': 'application/json',
+  },
 });
 
 const api = {
-    getItems: (url: string, params: any) => {
-        // console.log('api.getItems params', params);
-        return instance({
-            url,
-            data: { ...params },
-        });
-    },
+  getItems: (url: string, params: any) => {
+    // console.log('api.getItems params', params);
+    return instance({
+      url,
+      data: { ...params },
+    });
+  },
 };
 
 export default api;
