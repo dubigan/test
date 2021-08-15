63dca9c65e10318d27bd04f93e77b8b1e0c79e02
ioak <353991@gmail.com>
2021-08-15 22:22:54 +0300

    Header

--- src/pages/owners/index.tsx
+++ src/pages/owners/index.tsx
@@ -3,11 +3,17 @@ import ListOfItems from "../../components/List/ListOfItems";
 import { TItemFunctions, TListOfItemsProps } from "../../components/List/ListTypes";
 import { TOwnerItem } from "../../components/Detail/DetailTypes";
 import { useListFunctions } from "../../components/List/useListFunctions";
+import Header from "../../components/Header/Header";
 
 const Owners = (props: TListOfItemsProps<TOwnerItem>) => {
     const functions = useListFunctions("owners");
 
-    return <ListOfItems<TOwnerItem> functions={functions as TItemFunctions<TOwnerItem>} />;
+    return (
+        <>
+            <Header />
+            <ListOfItems<TOwnerItem> functions={functions as TItemFunctions<TOwnerItem>} />;
+        </>
+    );
 };
 
 export default Owners;
