2023ef56486faa463f030508e339e0dd2f76c2ac
ioak <353991@gmail.com>
2021-08-20 00:23:39 +0300

    Link in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Link from "next/link";
 
 export class Header extends Component {
-  render() {
-    return (
-      <nav className="navbar">
-        {/* <button
+    render() {
+        return (
+            <nav className="navbar">
+                {/* <button
           className="navbar-toggler"
           type="button"
           data-toggle="collapse"
@@ -15,21 +16,21 @@ export class Header extends Component {
         >
           <span className="navbar-toggler-icon"></span>
         </button> */}
-        {/* <div className="collapse navbar-collapse" id="navbarTogglerDemo01"> */}
-        <a className="navbar__item" href="/owners">
-          Автовладельцы
-        </a>
-        <a className="navbar__item navbar__item_cars" href="/cars">
-          Автомобили
-        </a>
-        <a className="navbar__item navbar__item_admin" href="/dashboard">
-          Админ
-        </a>
-        {/* <ul className="navbar-nav mr-auto mt-2 mt-lg-0"></ul> */}
-        {/* </div> */}
-      </nav>
-    );
-  }
+                {/* <div className="collapse navbar-collapse" id="navbarTogglerDemo01"> */}
+                <div className="navbar_item">
+                    <Link href="/owners">Автовладельцы</Link>
+                </div>
+                <div className="navbar_item navbar__item_cars">
+                    <Link href="/cars">Автомобили</Link>
+                </div>
+                <div className="navbar_item navbar__item_admin">
+                    <Link href="/dashboard">Админ</Link>
+                </div>
+                {/* <ul className="navbar-nav mr-auto mt-2 mt-lg-0"></ul> */}
+                {/* </div> */}
+            </nav>
+        );
+    }
 }
 
 export default Header;
