03315a16d7c15020afd9aa3e9e9be83d5d9cbfe1
ioak <353991@gmail.com>
2021-08-19 23:58:47 +0300

    Loader @ts-ignore

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import * as styles from "../css/Loader.module.css";
 
 const Loader = () => (
+    // @ts-ignore
     <div className={styles.centerLoader}>
+        {/* @ts-ignore */}
         <div className={styles.ldsDualRing} />
     </div>
 );
